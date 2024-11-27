1f06d9d0355f37055da000a77282c4116e1dd05a
depilz <depilz@github.com>
2024-11-27 19:36:35 -0300

    Beta version

--- plugins/2020.2600/iphone/metadata.lua
+++ plugins/2020.2600/iphone/metadata.lua
@@ -3,19 +3,10 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-
-		-- This is the name without the 'lib' prefix.
-		-- In this case, the static library is called: libplugin_spine.a
-		staticLibs = { 'plugin_spine', }, 
-
+		staticLibs = { 'plugin_spine', },
 		frameworks = {},
 		frameworksOptional = {},
-	},
-	coronaManifest = {
-		dependencies = {
-			-- Example dependencies:
-			--["plugin.memoryBitmap"] = "com.coronalabs",
-		},
+		-- usesSwift = true,
 	},
 }
 
