d3593c710bed1b219ea713c27609288d7146ec85
depilz <depilz@github.com>
2024-12-31 09:43:13 -0400

    Important bugfixes

--- plugins/2020.2600/iphone-sim/metadata.lua
+++ plugins/2020.2600/iphone-sim/metadata.lua
@@ -3,19 +3,10 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-
-		-- This is the name without the 'lib' prefix.
-		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = { 'plugin_spine', }, 
-
+		staticLibs = { 'plugin_spine', },
 		frameworks = {},
 		frameworksOptional = {},
-	},
-	coronaManifest = {
-		dependencies = {
-			-- Example dependencies:
-			--["plugin.memoryBitmap"] = "com.coronalabs",
-		},
+		-- usesSwift = true,
 	},
 }
 
